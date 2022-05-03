c721d03c32701811d90d044451aee7ae61bd63a6
dkhan8771 <92078708+dkhan8771@users.noreply.github.com>
2022-05-04 01:01:44 +0530

    Update PropertyDemo.java

--- PropertyDemo.java
+++ PropertyDemo.java
@@ -20,7 +20,7 @@ public class PropertyDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Properties p=new Properties();
+		Properties p = new Properties();
 		
 		p.setProperty("Brand", 		"Dell");
 		p.setProperty("OS", 		"Windows");
@@ -29,45 +29,30 @@ public class PropertyDemo {
 		p.setProperty("RAM",   		"8GB");
 		p.setProperty("Hard Disk", 	"1TB");
 		p.setProperty("Battery", 	"Lithium 5000MAH");
+		p.setProperty("Cell", 		"4 Cell");
+		p.setProperty("Backup", 		"up to 8 hrs");
 		p.setProperty("camera", 	"2 Mega Pixel");
 		p.setProperty("Display", 	"full hd");
-		p.setProperty("Resolution", "1680x2040");
-		p.setProperty("Microphone", "Yes");
+		p.setProperty("Resolution", 	"1680x2040");
+		p.setProperty("Touch Screen", 	"Yes");
+		p.setProperty("Rotation", 	"180 Degree");
+		p.setProperty("Microphone", 	"Yes");
 		p.setProperty("Speaker", 	"Duel Strero");
 		p.setProperty("Blutooth", 	"0.5");
 		p.setProperty("wifi", 		"0.5");
 		p.setProperty("Port", 		"2 USB3.0");
+		p.setProperty("HeadPhone jack", "Yes 2.0");
+		p.setProperty("Bright Keyboard", "Yes");
+		p.setProperty("Charger", 	"Ac Adapter");
+		p.setProperty("Fast Charging", 	"Yes");
+		p.setProperty("Pen", 		"No");
+		p.setProperty("Fingerprint", 	"No");
 		p.setProperty("Warrenty", 	"2Years(Domestic)");
 		
 		
 		//Method to generate the text file
-//		try {
-//			p.store(new FileOutputStream("E:\\ProgramData/data.txt"), "Laptop_Details");
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-
-		
-		//Method to generate the XML file
-//		try {
-//			p.storeToXML(new FileOutputStream("E:\\ProgramData/MyData.xml"), "Laptop Details");
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-		
-	
-		//Method to read the text file
 		try {
-			p.load(new FileInputStream("E:\\ProgramData/data.txt"));
+			p.store(new FileOutputStream("E:\\ProgramData/data.txt"), "Laptop_Details");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -75,7 +60,32 @@ public class PropertyDemo {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(p);
+
+		
+		//Method to generate the XML file
+		try {
+			p.storeToXML(new FileOutputStream("E:\\ProgramData/MyData.xml"), "Laptop Details");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	
+		//Method to read the text file
+// 		try {
+// 			p.load(new FileInputStream("E:\\ProgramData/data.txt"));
+// 		} catch (FileNotFoundException e) {
+// 			// TODO Auto-generated catch block
+// 			e.printStackTrace();
+// 		} catch (IOException e) {
+// 			// TODO Auto-generated catch block
+// 			e.printStackTrace();
+// 		}
+// 		System.out.println(p);
 		
 		
 		
